72f9a28f4f55af9f78fb909cf8b9392fd6ac33e5
tommyshere <127OTIQaSz%e>
2022-05-26 00:40:18 -0700

    eslint and prettier

--- src/app/views/home/leaderboard/leaderboard.component.ts
+++ src/app/views/home/leaderboard/leaderboard.component.ts
@@ -1,29 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
-import {OverallIndScore} from '../../../interface';
-import {LeaderboardService} from './leaderboard.service';
-
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { OverallIndScore } from '../../../interface';
+import { LeaderboardService } from './leaderboard.service';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.scss']
+  styleUrls: ['./leaderboard.component.scss'],
 })
 export class LeaderboardComponent implements OnInit {
   public leaderboard!: OverallIndScore[];
-  public displayedColumns: string[] = ['name', 'thursday', 'friday', 'saturday', 'sunday', 'overall'];
+  public displayedColumns: string[] = [
+    'name',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+    'overall',
+  ];
   private _rx!: Subscription;
 
-  constructor(
-    private leaderboardService: LeaderboardService
-  ) {
-
-  }
+  constructor(private leaderboardService: LeaderboardService) {}
 
   ngOnInit(): void {
     this._rx = this.leaderboardService.getLeaderboard().subscribe(data => {
       this.leaderboard = data;
     });
   }
-
 }
